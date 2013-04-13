37d41472ea9336b889a980787c8acda773cf4fe0
Kazuhiro Osawa <yappo@shibuya.pl>
2013-04-13 20:40:21 +0900

    tidy

--- lib/MFPM/Web/C/Root.pm
+++ lib/MFPM/Web/C/Root.pm
@@ -5,12 +5,12 @@ use warnings;
 sub index {
     my($self, $c) = @_;
     return $c->render('index.tt');
-};
+}
 
 sub logout {
     my($self, $c) = @_;
     $c->session->expire();
     return $c->redirect('/');
-};
+}
 
 1;
